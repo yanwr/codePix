ea4893cf692a3a7a13dc34b10f6e6bdd80c92424
Yan Weschenfelder Rodrigues <yanywr@gmail.com>
2022-05-07 18:16:48 -0300

    #11 - PixKey Controller made. Configured docker-compose to connect applications between containers

--- bank-service/src/controllers/pix-key/pix-key.controller.ts
+++ bank-service/src/controllers/pix-key/pix-key.controller.ts
@@ -1,4 +1,31 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, ParseUUIDPipe, Post } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { PixKey } from 'src/models/pix-key.model';
+import { Repository } from 'typeorm';
 
-@Controller('pix-keys')
-export class PixKeyController {}
+@Controller('pix-keys/:bankAccountId')
+export class PixKeyController {
+  constructor(
+    @InjectRepository(PixKey)
+    private pixKeyRepository: Repository<PixKey>
+  ) {}
+
+  @Get()
+  getAllPixKeys(@Param("bankAccountId", new ParseUUIDPipe({ version: "4"})) bankAccountId: string) {
+    return this.pixKeyRepository.find({
+      where: { bankAccountId },
+      order: { createdAt: "DESC" }
+    });
+  }
+
+  @Post()
+  createAndSavaPixKey() {
+
+  }
+
+  @Get("exists")
+  isTherePixKey() {
+
+  }
+
+}
